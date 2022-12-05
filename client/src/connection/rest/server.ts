120c1ed6ad0ab72d8135d3526a9c326231e36c65
Joseph Henry <Joseph.Henry@sas.com>
2022-12-05 09:46:16 -0500

    feat: Implemented SAS Log Streaming (#39).
    Now the SAS log will be updated as the SAS code is executing.
    This gives a better view to the user as to where in the process
    the SAS code is.
    
    This also fixes the issue (#36) where a job that is longer than
    60 seconds results in an error in vscode.
    
    Signed-off-by: Joseph Henry <Joseph.Henry@sas.com>

---
+++ client/src/connection/rest/server.ts
@@ -0,0 +1,123 @@
+import { BaseCompute, Compute, getApiConfig, stateOptions } from "./common";
+import { ServersApi, Server, Link } from "./api/compute";
+import { ComputeSession } from "./session";
+import { AxiosResponse } from "axios";
+//import { ComputeSession } from "./session";
+
+export class ComputeServer extends Compute {
+  api;
+  _self: Server & BaseCompute;
+
+  constructor(id: string) {
+    super();
+
+    this._self = { id: id };
+
+    this.api = ServersApi(getApiConfig());
+  }
+
+  get id(): string {
+    return this._self?.id || "";
+  }
+
+  get links(): Array<Link> {
+    return this._self?.links || [];
+  }
+
+  static fromInterface(server: Server): ComputeServer {
+    const _server = new ComputeServer("");
+    _server._self = server;
+    return _server;
+  }
+
+  static fromResponse(response: AxiosResponse): ComputeServer {
+    const _server = ComputeServer.fromInterface(response.data);
+    _server.etag = response.headers.etag;
+    return _server;
+  }
+
+  async self<Server>(): Promise<Server> {
+    if (this._self.id === undefined)
+      throw new Error("Cannot call self on object with no id");
+
+    const res = await this.api.getServer({ serverId: this.id });
+    if (res.status === 200) {
+      this._self = res.data;
+      this.etag = res.headers["etag"];
+      return res.data;
+    } else {
+      throw new Error(
+        `Error getting server with ID  ${this.id} - ${res.message}`
+      );
+    }
+  }
+
+  async createSession(): Promise<ComputeSession> {
+    if (this._self.links === undefined) await this.self();
+
+    const link = this.getLink(this.links, "createSession");
+    if (link === undefined)
+      throw new Error("Server does not have createSession link");
+
+    //Create the session
+    //TODO: Add session create options
+    //TODO: Session request should be an interface
+    const body = {
+      version: 1,
+      name: "mysess",
+      description: "This is a session",
+      attributes: {},
+      environment: {
+        options: ["-validmemname EXTEND", "-validvarname ANY", "-memsize 0"],
+        autoExecLines: [],
+      },
+    };
+
+    const resp = await this.requestLink(link, { data: body });
+
+    if (!(resp.status === 201 || resp.status === 200)) {
+      throw new Error(<string>resp?.data);
+    }
+
+    //Create the session from the http resposne
+    const session = ComputeSession.fromResponse(resp);
+    return session;
+  }
+
+  async getSession(sessionId?: string): Promise<ComputeSession> {
+    if (sessionId !== undefined) {
+      return new ComputeSession(sessionId);
+    } else {
+      return this.createSession();
+    }
+  }
+
+  async getState(options?: stateOptions): Promise<string> {
+    if (this._self.links === undefined) await this.self();
+
+    const params = {};
+    const headers = {};
+
+    if (options !== undefined) {
+      if (options.onChange) {
+        headers["If-None-Match"] = this.etag;
+      }
+
+      if (options.wait) params["timeout"] = options.wait;
+    }
+
+    const link = this.getLink(this._self.links, "state");
+    if (link === undefined) throw new Error("Server does not have state link");
+
+    const { data, status } = await this.requestLink(link, {
+      params: params,
+      headers: headers,
+    });
+
+    if (status === 200) {
+      return <string>data;
+    } else {
+      throw new Error("Something went wrong");
+    }
+  }
+}
