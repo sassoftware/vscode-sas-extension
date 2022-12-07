f3e74298c58237c8491e19705381d438394727db
Scott Dover <Scott.Dover@sas.com>
2022-12-07 15:37:26 -0500

    Update eslint suppression

--- server/src/browser/ResLoader.ts
+++ server/src/browser/ResLoader.ts
@@ -6,13 +6,13 @@ export const ResLoader = {
   get: function (url: string, cb: (arg0: any) => void, async?: boolean) {
     // have to explicitly write path for webpack to bundle
     const index = url.indexOf("/data/");
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
     if (index > 0) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
       cb(require(`../../data/${url.slice(index + 6)}`));
     } else {
       const index = url.indexOf("/pubsdata/");
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
       if (index > 0) {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
         cb(require(`../../pubsdata/${url.slice(index + 10)}`));
       }
     }
