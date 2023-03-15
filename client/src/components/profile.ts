32d34f4cf35b086d0ecc2c0e8631d4babfa9dc18
Joseph Henry <Joseph.Henry@sas.com>
2023-03-15 16:03:35 -0400

    Add serverId to profile.ts
    Signed-off-by: Joseph Henry <Joseph.Henry@sas.com>

--- client/src/components/profile.ts
+++ client/src/components/profile.ts
@@ -57,6 +57,12 @@ export enum ConnectionType {
  * Profile is an interface that represents a users profile.  Currently
  * supports two different authentication flows, token and password
  * flow with the clientId and clientSecret.
+ *
+ * Direct connect is also supported where a server is already started with
+ * a static serverId. Setting serverId in the profile indicates that a connection
+ * to that specific server with Id will be created. This overrides the context
+ * value. Normally this option should not be set by the user since it is most likely
+ * being set by an automated process.
  */
 export interface ViyaProfile {
   connectionType: ConnectionType.Rest;
@@ -64,6 +70,7 @@ export interface ViyaProfile {
   clientId?: string;
   clientSecret?: string;
   context?: string;
+  serverId?: string;
 }
 
 export interface SSHProfile {
