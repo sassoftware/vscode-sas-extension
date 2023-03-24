69de0e8e12f16975902807fa83f446d5098da752
Scott Dover <Scott.Dover@sas.com>
2023-03-24 15:00:48 -0400

    fix eslint/tsconfig skipping

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: ["client/src/webview/*"],
   rules: {
     eqeqeq: "error",
     "prefer-const": "error",
