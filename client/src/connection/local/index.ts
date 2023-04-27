ff7aa10d4601d791468c71923a47d5f3faee37d2
Joe Morris <Joe.Morris@sas.com>
2023-04-26 22:42:46 -0400

    cleanup

--- client/src/connection/local/index.ts
+++ client/src/connection/local/index.ts
@@ -2,6 +2,7 @@ import { LogLine, RunResult, Session } from "..";
 import { spawn } from "child_process";
 import * as fs from "fs";
 import * as os from "os";
+import * as path from "path";
 
 let config: Config;
 let logLines: LogLine[] = [];
@@ -9,6 +10,7 @@ let tmpDir: string;
 let workDirectory: string;
 let tmpProgramName: string;
 const programName = "program";
+let programFullName: string;
 let logFileName: string;
 let listFileName: string;
 let odsHtmlFileName = "";
@@ -22,11 +24,16 @@ const close = (): void | Promise<void> => {
   logLines = [];
 
   // Delete the temporary files
-  fs.unlinkSync(programName);
-  fs.unlinkSync(logFileName);
-  fs.unlinkSync(odsHtmlFileName);
-  fs.unlinkSync(listFileName);
-  fs.unlinkSync(workDirectory);
+  try {
+    fs.unlinkSync(programFullName);
+    fs.unlinkSync(logFileName);
+    fs.unlinkSync(odsHtmlFileName);
+    fs.unlinkSync(listFileName);
+    fs.rmdirSync(workDirectory);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+  return Promise.resolve();
 };
 
 const sessionId = (): string | undefined => {
@@ -35,85 +42,104 @@ const sessionId = (): string | undefined => {
 
 const setup = (): Promise<void> => {
   tmpDir = process.env.TEMP || os.tmpdir();
-  workDirectory = `${tmpDir}/${process.pid}`;
+  workDirectory = path.join(tmpDir, `${process.pid}`);
   tmpProgramName = "program";
-  logFileName = `${workDirectory}/${tmpProgramName}.log`;
-  listFileName = `${workDirectory}/${tmpProgramName}.lst`;
-  return;
+  logFileName = path.join(workDirectory, `${tmpProgramName}.log`);
+  listFileName = path.join(workDirectory, `${tmpProgramName}.lst`);
+  programFullName = path.join(workDirectory, `${tmpProgramName}.sas`);
+  return Promise.resolve();
 };
 
-const run = (
+const run = async (
   code: string,
   onLog?: (logs: LogLine[]) => void
 ): Promise<RunResult> => {
+  const codeWithODSPath = code.replace(
+    "ods html5;",
+    `ods html5 path="${workDirectory}";`
+  );
+
   const programOpts = [
-    `${programName}`,
+    `-sysin "${programFullName}"`,
     "-batch",
     "-terminal",
     "-nosyntaxcheck",
     "-pagesize MAX",
     "-nosplash",
-    `-work ${workDirectory}`,
-    `-log ${logFileName}`,
-    `-print ${listFileName}`,
+    "-nologo",
+    `-work "${workDirectory}"`,
+    `-log "${logFileName}"`,
+    `-print "${listFileName}"`,
   ];
+
+  fs.mkdirSync(workDirectory, {recursive:true});
+  fs.writeFileSync(programFullName, codeWithODSPath);
 
   //add user opts to default opts
   programOpts.push(...config.sasOptions);
+  const programOptsStr = programOpts.join(" ");
 
   //launch sas executable
-  const childProcess = spawn(`${config.sasPath} ${programOpts}`);
+
+  const childProcess = spawn(`${config.sasPath} ${programOptsStr}`, {
+    shell: true,
+    cwd: workDirectory,
+  });
 
   childProcess.stdout.on("data", (chunk) => {
     logLines.push({ type: "normal", line: chunk });
     onLog(logLines);
+    throw new Error("failed");
   });
 
   childProcess.stderr.on("data", (err: Error) => {
     logLines.push({ type: "error", line: err.message });
     onLog(logLines);
+    throw new Error("failed");
   });
 
-  childProcess.on("close", (code) => {
-    if (code === 0) {
-      // SAS process completed successfully
-      let odsHtmlFileContents: string;
-      const logFileContents = fs.readFileSync(logFileName, "utf8");
-      const lines = logFileContents.split(/\r?\n/);
+  const rc = await new Promise((resolve, reject) => {
+    childProcess.on("exit", resolve);
+    childProcess.on("error", reject);
+  });
 
-      for (const line of lines) {
-        logLines.push({
-          line: line,
-          type: line.startsWith("ERROR:") ? "error" : "normal",
-        });
+  let runResult: RunResult;
+  if (rc === 0) {
+    // SAS process completed successfully
+    let odsHtmlFileContents: string;
+    const logFileContents = fs.readFileSync(logFileName, "utf8");
+    const lines = logFileContents.split(/\r?\n/);
 
-        onLog(logLines);
-
-        const filename =
-          line.match(/NOTE: .+ HTML5.* Body .+: (.+)\.htm/)?.[1] ?? "";
-
-        if (filename) {
-          odsHtmlFileName = `${workDirectory}/${filename}.htm`;
-          odsHtmlFileContents = fs.readFileSync(odsHtmlFileName, {
-            encoding: "utf8",
-            flag: "r",
-          });
-          break;
-        }
-      }
-
-      const runResult: RunResult = {
-        html5: odsHtmlFileContents,
-        title: "Results",
+    for (const line of lines) {
+      const logline: LogLine = {
+        line: line,
+        type: line.startsWith("ERROR:") ? "error" : "normal",
       };
+      logLines.push(logline);
 
-      return runResult;
-    } else {
-      throw new Error("Execution failed. See log.");
+      onLog([logline]);
+
+      const filename =
+        line.match(/NOTE: .+ HTML5.* Body .+: (.+)\.htm/)?.[1] ?? "";
+
+      if (filename) {
+        odsHtmlFileName = `${workDirectory}/${filename}.htm`;
+        odsHtmlFileContents = fs.readFileSync(odsHtmlFileName, {
+          encoding: "utf8",
+          flag: "r",
+        });
+      }
     }
-  });
 
-  return;
+    runResult = {
+      html5: odsHtmlFileContents,
+      title: "Results",
+    };
+  } else {
+    throw new Error("Execution failed. See log.");
+  }
+
+  return runResult;
 };
 
 export const getSession = (c: Config): Session => {
