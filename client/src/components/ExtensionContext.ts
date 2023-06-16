32e7031929421ed5e984139685da98b990c7878c
Scott Dover <Scott.Dover@sas.com>
2023-06-16 09:44:45 -0400

    run npm run copyright:check --fix

--- client/src/components/ExtensionContext.ts
+++ client/src/components/ExtensionContext.ts
@@ -1,3 +1,6 @@
+// Copyright © 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { ExtensionContext } from "vscode";
 
 let context: ExtensionContext;
