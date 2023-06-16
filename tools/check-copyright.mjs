37d822019bd0e2902f887fbf7c53ca7450f8045d
Scott Dover <Scott.Dover@sas.com>
2023-06-16 09:38:22 -0400

    fix copyright check script

--- tools/check-copyright.mjs
+++ tools/check-copyright.mjs
@@ -1,34 +1,49 @@
-import { exec } from "child_process";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
+import glob from "glob";
 
-const INCLUDED_FILE_TYPES = /.*\.(mjs|js|ts|tsx|jsx)$/;
-const EXCLUDED_FILE_TYPES = /.*(\.test\.tsx?|check-copyright\.mjs)$/;
+// These files will not be checked for copyright information
+const filesToIgnore = [
+  "**/node_modules/**",
+  "**/out/**",
+  "**/dist/**",
+  "tools/*",
+  "*.config.js",
+  "*.test.tsx?",
+  "**/test/**",
+];
+
 const COPYRIGHT_REGEX = /^\/\/ Copyright © ([0-9-\s]+), SAS Institute/;
+const COPYRIGHT_TEMPLATE = `// Copyright © ${new Date().getFullYear()}, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
-const processChanges = (changedFiles) => {
-  const filesToCheck = changedFiles
-    .map((file) => file.trim())
-    .filter(
-      (file) =>
-        INCLUDED_FILE_TYPES.test(file) && !EXCLUDED_FILE_TYPES.test(file)
-    );
+`;
 
+const processChanges = (filesToCheck, fix = false) => {
   let invalidFiles = [];
   filesToCheck.map((file) => {
     const fileContents = readFileSync(file);
     if (!COPYRIGHT_REGEX.test(fileContents.toString())) {
       invalidFiles.push(file);
+      if (fix) {
+        writeFileSync(file, `${COPYRIGHT_TEMPLATE}${fileContents}`);
+      }
     }
   });
 
   if (invalidFiles.length > 0) {
-    console.log("The following files are missing copyright information");
+    console.log(
+      fix
+        ? "The following files have been updated with copyright information"
+        : "The following files are missing copyright information"
+    );
     console.log(invalidFiles.map((file) => `- ${file}`).join("\n"));
     process.exit(1);
   }
 };
 
-await exec(
-  `git diff origin/HEAD --diff-filter=A --name-only`,
-  async (error, stdout, stderr) => processChanges(stdout.split("\n"))
+await processChanges(
+  glob.sync("**/*.{mjs,js,ts,tsx,jsx}", {
+    ignore: filesToIgnore,
+  }),
+  process.env.npm_config_fix || false
 );
