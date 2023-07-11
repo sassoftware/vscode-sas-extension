0ec59dc36fa696829ea766a0619abe191c097761
Scott Dover <Scott.Dover@sas.com>
2023-07-11 15:48:22 -0400

    initial adding of things

--- client/src/webview/DataViewer.tsx
+++ client/src/webview/DataViewer.tsx
@@ -8,16 +8,34 @@ import {
   VSCodeDataGridCell,
   VSCodeDataGridRow,
 } from "@vscode/webview-ui-toolkit/react";
+import { AgGridReact } from "ag-grid-react";
 import * as React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import InfiniteScroll from "react-infinite-scroller";
 import useDataViewer from "./useDataViewer";
 
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+const { useState } = React;
+
 const DataViewer = () => {
   const { loadMoreResults, headers, rows, hasMore } = useDataViewer();
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 },
+  ]);
+
+  const [columnDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+  ]);
 
   return (
     <div>
+      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
       <InfiniteScroll
         pageStart={0}
         loadMore={loadMoreResults}
