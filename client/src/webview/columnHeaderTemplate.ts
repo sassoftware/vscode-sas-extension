4489cb00c34e1b19cea5332b2a89d0ae311d1438
Scott Dover <Scott.Dover@sas.com>
2023-07-17 08:50:30 -0400

    add copyright

--- client/src/webview/columnHeaderTemplate.ts
+++ client/src/webview/columnHeaderTemplate.ts
@@ -1,3 +1,6 @@
+// Copyright © 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 const getIconForColumnType = (type: string) => {
   switch (type) {
     case "FLOAT":
