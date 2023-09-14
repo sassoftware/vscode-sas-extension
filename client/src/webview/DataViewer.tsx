7d8a56b48f57bf79cbaaf465b98f5f068977d7d8
Scott Dover <Scott.Dover@sas.com>
2023-09-14 15:46:43 -0400

    Update theming based on kind

--- client/src/webview/DataViewer.tsx
+++ client/src/webview/DataViewer.tsx
@@ -1,6 +1,6 @@
 // Copyright © 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { createElement, useState } from "react";
+import { createElement, useMemo } from "react";
 import { createRoot } from "react-dom/client";
 
 import { AgGridReact } from "ag-grid-react";
@@ -21,11 +21,20 @@ const gridStyles = {
 
 const DataViewer = () => {
   const { columns, onGridReady } = useDataViewer();
-  const [theme] = useState(
-    document.querySelector(".vscode-dark")
-      ? "ag-theme-alpine-dark"
-      : "ag-theme-alpine",
-  );
+  const theme = useMemo(() => {
+    const themeKind = document
+      .querySelector("[data-vscode-theme-kind]")
+      .getAttribute("data-vscode-theme-kind");
+
+    switch (themeKind) {
+      case "vscode-high-contrast-light":
+      case "vscode-light":
+        return "ag-theme-alpine";
+      case "vscode-high-contrast":
+      case "vscode-dark":
+        return "ag-theme-alpine-dark";
+    }
+  }, []);
 
   if (columns.length === 0) {
     return null;
