1fdf23d2f0788dd6a7cb3d636a2f686ec854e8f1
Scott Dover <Scott.Dover@sas.com>
2024-02-21 10:36:08 -0500

    add copyright

--- client/src/connection/itc/const.ts
+++ client/src/connection/itc/const.ts
@@ -1,3 +1,5 @@
+// Copyright © 2024, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { l10n } from "vscode";
 
 export const ERROR_START_TAG = '<ITCError>';
