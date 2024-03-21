1a5fba226fd10486f07201912db0fa7efdd5d1d5
Shuguang Sun <shuguang79@qq.com>
2024-03-21 16:18:21 +0800

    chore: saspy
    Signed-off-by: Shuguang Sun <shuguang79@qq.com>

--- client/src/connection/saspy/script.ts
+++
@@ -1,9 +0,0 @@
-// Copyright © 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-// import { LineCodes } from "./types";
-
-export const scriptContent = `
-import saspy
-import warnings
-
-`;
