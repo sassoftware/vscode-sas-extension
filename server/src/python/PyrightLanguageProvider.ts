4c62d855b59c78e8527d6c89f086331c4e279225
Jonas(Jiajie) Jing <Jiajie.Jing@sas.com>
2024-06-05 10:55:52 +0800

    support browser

--- server/src/python/PyrightLanguageProvider.ts
+++ server/src/python/PyrightLanguageProvider.ts
@@ -1,6 +1,7 @@
 // Copyright © 2022-2024, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import {
+import type {
+  CallHierarchyIncomingCall,
   CallHierarchyIncomingCallsParams,
   CallHierarchyItem,
   CallHierarchyOutgoingCall,
@@ -10,7 +11,6 @@ import {
   CompletionItem,
   CompletionList,
   CompletionParams,
-  Connection,
   Declaration,
   DeclarationLink,
   Definition,
@@ -24,241 +24,60 @@ import {
   DocumentSymbol,
   DocumentSymbolParams,
   ExecuteCommandParams,
+  Hover,
   HoverParams,
   InitializeParams,
   InitializeResult,
-  LSPAny,
   Location,
   PrepareRenameParams,
+  Range,
   ReferenceParams,
   RenameParams,
   ResultProgressReporter,
+  SignatureHelp,
   SignatureHelpParams,
   SymbolInformation,
+  TextDocument,
   TextDocumentPositionParams,
   WorkDoneProgressReporter,
+  WorkspaceEdit,
   WorkspaceSymbol,
   WorkspaceSymbolParams,
 } from "vscode-languageserver";
-import { TextDocument } from "vscode-languageserver-textdocument";
 
-import { IPythonMode } from "pyright-internal-lsp/dist/packages/pyright-internal/src/analyzer/sourceFile";
-import {
-  FileSystem,
-  ReadOnlyFileSystem,
-} from "pyright-internal-lsp/dist/packages/pyright-internal/src/common/fileSystem";
-import { DocumentRange } from "pyright-internal-lsp/dist/packages/pyright-internal/src/common/textRange";
-import { Uri } from "pyright-internal-lsp/dist/packages/pyright-internal/src/common/uri/uri";
-import { ClientCapabilities } from "pyright-internal-lsp/dist/packages/pyright-internal/src/languageServerBase";
-import { CollectionResult } from "pyright-internal-lsp/dist/packages/pyright-internal/src/languageService/documentSymbolCollector";
-import { ParseFileResults } from "pyright-internal-lsp/dist/packages/pyright-internal/src/parser/parser";
-import { PyrightServer } from "pyright-internal-lsp/dist/packages/pyright-internal/src/server";
+import type { IPythonMode } from "pyright-internal-node/dist/packages/pyright-internal/src/analyzer/sourceFile";
+import type { ReadOnlyFileSystem } from "pyright-internal-node/dist/packages/pyright-internal/src/common/fileSystem";
+import type { ClientCapabilities } from "pyright-internal-node/dist/packages/pyright-internal/src/common/languageServerInterface";
+import type { DocumentRange } from "pyright-internal-node/dist/packages/pyright-internal/src/common/textRange";
+import type { Uri } from "pyright-internal-node/dist/packages/pyright-internal/src/common/uri/uri";
+import type { CollectionResult } from "pyright-internal-node/dist/packages/pyright-internal/src/languageService/documentSymbolCollector";
+import type { ParseFileResults } from "pyright-internal-node/dist/packages/pyright-internal/src/parser/parser";
 
-import { CodeZoneManager } from "../sas/CodeZoneManager";
 import { LanguageServiceProvider } from "../sas/LanguageServiceProvider";
 
-export class PyrightLanguageProvider extends PyrightServer {
-  protected sasLspProvider: (uri: string) => LanguageServiceProvider;
-
-  constructor(
-    connection: Connection,
-    maxWorkers: number,
-    sasLspProvider: (uri: string) => LanguageServiceProvider,
-    realFileSystem?: FileSystem,
-  ) {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    super({ ...connection, listen() {} } as LSPAny, maxWorkers, realFileSystem);
-    this.sasLspProvider = sasLspProvider;
-  }
-
-  public getClientCapabilities(): ClientCapabilities {
-    return this.client;
-  }
-
-  protected extractPythonCodes(doc: TextDocument): string {
-    const uri = doc.uri;
-    const languageService = this.sasLspProvider(uri);
-    const codeZoneManager = languageService.getCodeZoneManager();
-    const pythonDocLines = [];
-    const symbols: DocumentSymbol[] = languageService.getDocumentSymbols();
-    for (let i = 0; i < symbols.length; i++) {
-      const symbol = symbols[i];
-      if (symbol.name?.toUpperCase() === "PROC PYTHON") {
-        let pythonCodeStart = undefined;
-        let pythonCodeEnd = undefined;
-        const pos = { ...symbol.range.start };
-        while (pos.line <= symbol.range.end.line) {
-          if (
-            !pythonCodeStart &&
-            codeZoneManager.getCurrentZone(pos.line, pos.character) ===
-              CodeZoneManager.ZONE_TYPE.EMBEDDED_LANG &&
-            // a codezone bug
-            pos.line >= symbol.range.start.line + 2
-          ) {
-            pythonCodeStart = { ...pos };
-          }
-          if (
-            pythonCodeStart &&
-            codeZoneManager.getCurrentZone(pos.line, pos.character) !==
-              CodeZoneManager.ZONE_TYPE.EMBEDDED_LANG
-          ) {
-            pythonCodeEnd = { ...pos };
-            if (pythonCodeEnd.character === 0) {
-              pythonCodeEnd.line--;
-              pythonCodeEnd.character =
-                languageService.model.getColumnCount(pythonCodeEnd.line) - 1;
-              if (pythonCodeEnd.character < 0) {
-                pythonCodeEnd.character = 0;
-              }
-            }
-            break;
-          }
-          pos.character++;
-          if (pos.character >= languageService.model.getLine(pos.line).length) {
-            pos.line++;
-            pos.character = 0;
-          }
-        }
-        if (!pythonCodeStart) {
-          continue;
-        }
-        const pythonCodeLines = doc
-          .getText({
-            start: pythonCodeStart,
-            end: pythonCodeEnd ?? symbol.range.end,
-          })
-          .split("\n");
-        let firstNotEmptyLine: string | undefined = undefined;
-        for (const line of pythonCodeLines) {
-          if (line.trim().length > 0 && !line.startsWith("#")) {
-            firstNotEmptyLine = line;
-            break;
-          }
-        }
-        const shouldAddDummyBlock: boolean =
-          !!firstNotEmptyLine && [" ", "\t"].includes(firstNotEmptyLine[0]);
-        const lineGap = pythonCodeStart.line - pythonDocLines.length;
-        for (let i = 0; i < lineGap; i++) {
-          if (shouldAddDummyBlock && i === lineGap - 1) {
-            pythonDocLines.push("if True:");
-          } else {
-            pythonDocLines.push("");
-          }
-        }
-        for (const line of pythonCodeLines) {
-          pythonDocLines.push(line);
-        }
-        pythonDocLines.push("pass");
-      }
-    }
-    const pythonDoc = pythonDocLines.join("\n");
-    return pythonDoc;
-  }
-
-  protected calculateMaxCommentIndent(docLines: string[]): number {
-    let minIndent: number = -1;
-    for (const line of docLines) {
-      if (line.trim().length === 0) {
-        continue;
-      }
-      let curIndent = 0;
-      for (let i = 0; i < line.length; i++) {
-        if (line[i] === " " || line[i] === "\t") {
-          curIndent++;
-        } else {
-          break;
-        }
-      }
-      if (minIndent === -1 || curIndent < minIndent) {
-        minIndent = curIndent;
-      }
-    }
-    return minIndent;
-  }
-
-  protected setupConnection(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    supportedCommands: string[],
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    supportedCodeActions: string[],
-  ): void {
-    return;
-  }
-
-  public onInitialized(): void {
-    this.updateSettingsForAllWorkspaces();
-    super.onInitialized();
-  }
-
-  public initialize(
+export interface PyrightLanguageProvider {
+  setSasLspProvider(provider: (uri: string) => LanguageServiceProvider): void;
+  initialize(
     params: InitializeParams,
     supportedCommands: string[],
     supportedCodeActions: string[],
-  ): InitializeResult {
-    return super.initialize(params, supportedCommands, supportedCodeActions);
-  }
-
-  public addContentChange(doc: TextDocument): void {
-    const pythonDoc = this.extractPythonCodes(doc);
-
-    this.onDidChangeTextDocument({
-      textDocument: doc,
-      contentChanges: [{ text: pythonDoc }],
-    });
-  }
-
-  public async onHover(params: HoverParams, token: CancellationToken) {
-    return await super.onHover(params, token);
-  }
-
-  public async onDidOpenTextDocument(
-    params: DidOpenTextDocumentParams,
-    ipythonMode = IPythonMode.None,
-  ) {
-    const doc = TextDocument.create(
-      params.textDocument.uri,
-      "sas",
-      params.textDocument.version,
-      params.textDocument.text,
-    );
-    const pythonDocContent = this.extractPythonCodes(doc);
-    const newParams: DidOpenTextDocumentParams = { ...params };
-    newParams.textDocument = { ...params.textDocument };
-    newParams.textDocument.languageId = "python";
-    newParams.textDocument.text = pythonDocContent;
-    await super.onDidOpenTextDocument(newParams, ipythonMode);
-  }
-
-  public async onDidCloseTextDocument(params: DidCloseTextDocumentParams) {
-    await super.onDidCloseTextDocument(params);
-  }
-
-  public onDidChangeConfiguration(params: DidChangeConfigurationParams): void {
-    super.onDidChangeConfiguration(params);
-  }
-
-  public async onDefinition(
+  ): Promise<InitializeResult>;
+  onInitialized(): void;
+  getClientCapabilities(): ClientCapabilities;
+  onDidChangeConfiguration(params: DidChangeConfigurationParams): void;
+  onDefinition(
     params: TextDocumentPositionParams,
     token: CancellationToken,
-  ): Promise<Definition | DefinitionLink[] | undefined | null> {
-    return await super.onDefinition(params, token);
-  }
-
-  public async onDeclaration(
+  ): Promise<Definition | DefinitionLink[] | undefined | null>;
+  onDeclaration(
     params: TextDocumentPositionParams,
     token: CancellationToken,
-  ): Promise<Declaration | DeclarationLink[] | undefined | null> {
-    return await super.onDeclaration(params, token);
-  }
-
-  public async onTypeDefinition(
+  ): Promise<Declaration | DeclarationLink[] | undefined | null>;
+  onTypeDefinition(
     params: TextDocumentPositionParams,
     token: CancellationToken,
-  ): Promise<Definition | DefinitionLink[] | undefined | null> {
-    return await super.onTypeDefinition(params, token);
-  }
-
-  public async onReferences(
+  ): Promise<Definition | DefinitionLink[] | undefined | null>;
+  onReferences(
     params: ReferenceParams,
     token: CancellationToken,
     workDoneReporter: WorkDoneProgressReporter,
@@ -272,111 +91,72 @@ export class PyrightLanguageProvider extends PyrightServer {
       fs: ReadOnlyFileSystem,
       ranges: DocumentRange,
     ) => Location | undefined,
-  ): Promise<Location[] | null | undefined> {
-    return await super.onReferences(
-      params,
-      token,
-      workDoneReporter,
-      resultReporter,
-      createDocumentRange,
-      convertToLocation,
-    );
-  }
-
-  public async onDocumentSymbol(
+  ): Promise<Location[] | null | undefined>;
+  onDocumentSymbol(
     params: DocumentSymbolParams,
     token: CancellationToken,
-  ): Promise<DocumentSymbol[] | SymbolInformation[] | null | undefined> {
-    return await super.onDocumentSymbol(params, token);
-  }
-
-  public async onWorkspaceSymbol(
+  ): Promise<DocumentSymbol[] | SymbolInformation[] | null | undefined>;
+  onWorkspaceSymbol(
     params: WorkspaceSymbolParams,
     token: CancellationToken,
     resultReporter: ResultProgressReporter<SymbolInformation[]> | undefined,
-  ): Promise<SymbolInformation[] | WorkspaceSymbol[] | null | undefined> {
-    return await super.onWorkspaceSymbol(params, token, resultReporter);
-  }
-
-  public async onDocumentHighlight(
+  ): Promise<SymbolInformation[] | WorkspaceSymbol[] | null | undefined>;
+  onHover(params: HoverParams, token: CancellationToken): Promise<Hover | null>;
+  onDocumentHighlight(
     params: DocumentHighlightParams,
     token: CancellationToken,
-  ): Promise<DocumentHighlight[] | null | undefined> {
-    if (params.position.character < 0) {
-      return null;
-    }
-    return await super.onDocumentHighlight(params, token);
-  }
-
-  public async onSignatureHelp(
+  ): Promise<DocumentHighlight[] | null | undefined>;
+  onSignatureHelp(
     params: SignatureHelpParams,
     token: CancellationToken,
-  ) {
-    return await super.onSignatureHelp(params, token);
-  }
-
-  public async onCompletion(
+  ): Promise<SignatureHelp | undefined | null>;
+  onCompletion(
     params: CompletionParams,
     token: CancellationToken,
-  ): Promise<CompletionList | null> {
-    return await super.onCompletion(params, token);
-  }
-
-  public async onCompletionResolve(
+  ): Promise<CompletionList | null>;
+  onCompletionResolve(
     params: CompletionItem,
     token: CancellationToken,
-  ): Promise<CompletionItem> {
-    return await super.onCompletionResolve(params, token);
-  }
-
-  public async onPrepareRenameRequest(
+  ): Promise<CompletionItem>;
+  onPrepareRenameRequest(
     params: PrepareRenameParams,
     token: CancellationToken,
-  ) {
-    return await super.onPrepareRenameRequest(params, token);
-  }
-
-  public async onRenameRequest(params: RenameParams, token: CancellationToken) {
-    if (params.position.character < 0) {
-      return null;
-    }
-    return await super.onRenameRequest(params, token);
-  }
-
-  public async onCallHierarchyPrepare(
+  ): Promise<
+    | Range
+    | {
+        range: Range;
+        placeholder: string;
+      }
+    | null
+  >;
+  onRenameRequest(
+    params: RenameParams,
+    token: CancellationToken,
+  ): Promise<WorkspaceEdit | null | undefined>;
+  onCallHierarchyPrepare(
     params: CallHierarchyPrepareParams,
     token: CancellationToken,
-  ): Promise<CallHierarchyItem[] | null> {
-    return await super.onCallHierarchyPrepare(params, token);
-  }
-
-  public async onCallHierarchyIncomingCalls(
+  ): Promise<CallHierarchyItem[] | null>;
+  onCallHierarchyIncomingCalls(
     params: CallHierarchyIncomingCallsParams,
     token: CancellationToken,
-  ) {
-    return await super.onCallHierarchyIncomingCalls(params, token);
-  }
-
-  public async onCallHierarchyOutgoingCalls(
+  ): Promise<CallHierarchyIncomingCall[] | null>;
+  onCallHierarchyOutgoingCalls(
     params: CallHierarchyOutgoingCallsParams,
     token: CancellationToken,
-  ): Promise<CallHierarchyOutgoingCall[] | null> {
-    return await super.onCallHierarchyOutgoingCalls(params, token);
-  }
-
-  public onDidChangeWatchedFiles(params: DidChangeWatchedFilesParams): void {
-    super.onDidChangeWatchedFiles(params);
-  }
-
-  public async onExecuteCommand(
+  ): Promise<CallHierarchyOutgoingCall[] | null>;
+  onDidOpenTextDocument(
+    params: DidOpenTextDocumentParams,
+    ipythonMode?: IPythonMode,
+  ): Promise<void>;
+  onDidCloseTextDocument(params: DidCloseTextDocumentParams): Promise<void>;
+  onDidChangeWatchedFiles(params: DidChangeWatchedFilesParams): void;
+  onShutdown(token: CancellationToken): Promise<void>;
+  onExecuteCommand(
     params: ExecuteCommandParams,
     token: CancellationToken,
     reporter: WorkDoneProgressReporter,
-  ) {
-    return await super.onExecuteCommand(params, token, reporter);
-  }
-
-  public async onShutdown(token: CancellationToken): Promise<void> {
-    return await super.onShutdown(token);
-  }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ): Promise<any>;
+  addContentChange(doc: TextDocument): void;
 }
--- server/src/sas/LanguageServiceProvider.ts
+++ server/src/sas/LanguageServiceProvider.ts
@@ -1,8 +1,11 @@
 // Copyright © 2022-2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { FoldingRange } from "vscode-languageserver";
+import {
+  DocumentSymbol,
+  FoldingRange,
+  SymbolKind,
+} from "vscode-languageserver";
 import { Range, TextDocument } from "vscode-languageserver-textdocument";
-import { DocumentSymbol, SymbolKind } from "vscode-languageserver-types";
 
 import { CodeZoneManager } from "./CodeZoneManager";
 import { CompletionProvider } from "./CompletionProvider";
