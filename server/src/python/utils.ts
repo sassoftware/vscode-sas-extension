128fa569c378f85ef4fd9e736115890e1348e509
Jonas(Jiajie) Jing <Jiajie.Jing@sas.com>
2024-07-26 17:21:58 +0800

    fix: unexpected indent error for comment only code
    Signed-off-by: Jonas(Jiajie) Jing <Jiajie.Jing@sas.com>

--- server/src/python/utils.ts
+++ server/src/python/utils.ts
@@ -15,71 +15,74 @@ export const extractPythonCodes = (
   const symbols: DocumentSymbol[] = languageService.getDocumentSymbols();
   for (let i = 0; i < symbols.length; i++) {
     const symbol = symbols[i];
-    if (symbol.name?.toUpperCase() === "PROC PYTHON") {
-      let pythonCodeStart = undefined;
-      let pythonCodeEnd = undefined;
-      const pos = { ...symbol.range.start };
-      while (pos.line <= symbol.range.end.line) {
-        if (
-          !pythonCodeStart &&
-          codeZoneManager.getCurrentZone(pos.line, pos.character) ===
-            CodeZoneManager.ZONE_TYPE.EMBEDDED_LANG &&
-          // a codezone bug
-          pos.line >= symbol.range.start.line + 2
-        ) {
-          pythonCodeStart = { ...pos };
-        }
-        if (
-          pythonCodeStart &&
-          codeZoneManager.getCurrentZone(pos.line, pos.character) !==
-            CodeZoneManager.ZONE_TYPE.EMBEDDED_LANG
-        ) {
-          pythonCodeEnd = { ...pos };
-          if (pythonCodeEnd.character === 0) {
-            pythonCodeEnd.line--;
-            pythonCodeEnd.character =
-              languageService.model.getColumnCount(pythonCodeEnd.line) - 1;
-            if (pythonCodeEnd.character < 0) {
-              pythonCodeEnd.character = 0;
-            }
+    if (symbol.name?.toUpperCase() !== "PROC PYTHON") {
+      continue;
+    }
+    let pythonCodeStart = undefined;
+    let pythonCodeEnd = undefined;
+    const pos = { ...symbol.range.start };
+    while (pos.line <= symbol.range.end.line) {
+      if (
+        !pythonCodeStart &&
+        codeZoneManager.getCurrentZone(pos.line, pos.character) ===
+          CodeZoneManager.ZONE_TYPE.EMBEDDED_LANG &&
+        // a codezone bug
+        pos.line >= symbol.range.start.line + 2
+      ) {
+        pythonCodeStart = { ...pos };
+      }
+      if (
+        pythonCodeStart &&
+        codeZoneManager.getCurrentZone(pos.line, pos.character) !==
+          CodeZoneManager.ZONE_TYPE.EMBEDDED_LANG
+      ) {
+        pythonCodeEnd = { ...pos };
+        if (pythonCodeEnd.character === 0) {
+          pythonCodeEnd.line--;
+          pythonCodeEnd.character =
+            languageService.model.getColumnCount(pythonCodeEnd.line) - 1;
+          if (pythonCodeEnd.character < 0) {
+            pythonCodeEnd.character = 0;
           }
-          break;
         }
-        pos.character++;
-        if (pos.character >= languageService.model.getLine(pos.line).length) {
-          pos.line++;
-          pos.character = 0;
-        }
+        break;
       }
-      if (!pythonCodeStart) {
-        continue;
+      pos.character++;
+      if (pos.character >= languageService.model.getLine(pos.line).length) {
+        pos.line++;
+        pos.character = 0;
       }
-      const pythonCodeLines = doc
-        .getText({
-          start: pythonCodeStart,
-          end: pythonCodeEnd ?? symbol.range.end,
-        })
-        .split("\n");
-      let firstNotEmptyLine: string | undefined = undefined;
-      for (const line of pythonCodeLines) {
-        if (line.trim().length > 0 && !line.startsWith("#")) {
-          firstNotEmptyLine = line;
-          break;
-        }
+    }
+    if (!pythonCodeStart) {
+      continue;
+    }
+    const pythonCodeLines = doc
+      .getText({
+        start: pythonCodeStart,
+        end: pythonCodeEnd ?? symbol.range.end,
+      })
+      .split("\n");
+    let firstNotEmptyLine: string | undefined = undefined;
+    for (const line of pythonCodeLines) {
+      if (line.trim().length > 0 && !line.trim().startsWith("#")) {
+        firstNotEmptyLine = line;
+        break;
       }
-      const shouldAddDummyBlock: boolean =
-        !!firstNotEmptyLine && [" ", "\t"].includes(firstNotEmptyLine[0]);
-      const lineGap = pythonCodeStart.line - pythonDocLines.length;
-      for (let i = 0; i < lineGap; i++) {
-        if (shouldAddDummyBlock && i === lineGap - 1) {
-          pythonDocLines.push("if True:");
-        } else {
-          pythonDocLines.push("");
-        }
+    }
+    const shouldAddDummyBlock: boolean =
+      !!firstNotEmptyLine && [" ", "\t"].includes(firstNotEmptyLine[0]);
+    const lineGap = pythonCodeStart.line - pythonDocLines.length;
+    for (let i = 0; i < lineGap; i++) {
+      if (shouldAddDummyBlock && i === lineGap - 1) {
+        pythonDocLines.push("if True:");
+      } else {
+        pythonDocLines.push("");
       }
-      for (const line of pythonCodeLines) {
-        pythonDocLines.push(line);
-      }
+    }
+    for (const line of pythonCodeLines) {
+      pythonDocLines.push(line);
+    }
+    if (firstNotEmptyLine) {
       pythonDocLines.push("pass");
     }
   }
