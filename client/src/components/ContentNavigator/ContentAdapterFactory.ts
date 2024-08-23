c614f32fee77e313dbdccdaba7adb201ff609b98
Scott Dover <Scott.Dover@sas.com>
2024-08-23 15:23:09 -0400

    chore: stub RestSASServerAdapter
    Signed-off-by: Scott Dover <Scott.Dover@sas.com>

--- client/src/components/ContentNavigator/ContentAdapterFactory.ts
+++ client/src/components/ContentNavigator/ContentAdapterFactory.ts
@@ -1,5 +1,6 @@
 // Copyright © 2024, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import RestSASServerAdapter from "../../connection/rest/RestSASServerAdapter";
 import SASContentAdapter from "../../connection/rest/SASContentAdapter";
 import { ConnectionType } from "../profile";
 import {
@@ -16,6 +17,8 @@ class ContentAdapterFactory {
   ): ContentAdapter {
     const key = `${connectionType}.${sourceType}`;
     switch (key) {
+      case `${ConnectionType.Rest}.${ContentSourceType.SASServer}`:
+        return new RestSASServerAdapter();
       case `${ConnectionType.Rest}.${ContentSourceType.SASContent}`:
       default:
         return new SASContentAdapter();
