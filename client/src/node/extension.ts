3da16fb991012b66d16c00ac85bc9f6dbf135107
Wei Wu <96556502+scnwwu@users.noreply.github.com>
2024-09-02 03:57:55 -0400

    feat: export sas notebook to sas file (#1157)

--- client/src/node/extension.ts
+++ client/src/node/extension.ts
@@ -51,6 +51,7 @@ import {
 import { LogTokensProvider, legend } from "../components/logViewer";
 import { sasDiagnostic } from "../components/logViewer/sasDiagnostics";
 import { NotebookController } from "../components/notebook/Controller";
+import { exportNotebook } from "../components/notebook/Exporter";
 import { NotebookSerializer } from "../components/notebook/Serializer";
 import { ConnectionType } from "../components/profile";
 import { SasTaskProvider } from "../components/tasks/SasTaskProvider";
@@ -194,6 +195,7 @@ export function activate(context: ExtensionContext): void {
     new NotebookController(),
     commands.registerCommand("SAS.notebook.new", newSASNotebook),
     commands.registerCommand("SAS.file.new", newSASFile),
+    commands.registerCommand("SAS.notebook.export", exportNotebook),
     tasks.registerTaskProvider(SAS_TASK_TYPE, new SasTaskProvider()),
     ...sasDiagnostic.getSubscriptions(),
   );
