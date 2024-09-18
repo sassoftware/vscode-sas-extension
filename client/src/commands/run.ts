72de76d00fcc4caef9b2d430a6bbf6c7f1eecda0
Wei Wu <96556502+scnwwu@users.noreply.github.com>
2024-09-18 01:07:11 -0400

    feat: clear log on execution start (#1168)
    Signed-off-by: Jonas(Jiajie) Jing <Jiajie.Jing@sas.com>

--- client/src/commands/run.ts
+++ client/src/commands/run.ts
@@ -12,10 +12,13 @@ import {
 } from "vscode";
 import type { BaseLanguageClient } from "vscode-languageclient";
 
+import { basename, extname } from "path";
+
 import { showResult } from "../components/ResultPanel";
 import {
   appendExecutionLogFn,
   appendSessionLogFn,
+  setFileName,
 } from "../components/logViewer";
 import { sasDiagnostic } from "../components/logViewer/sasDiagnostics";
 import { SASCodeDocument } from "../components/utils/SASCodeDocument";
@@ -110,6 +113,9 @@ async function runCode(selected?: boolean, uri?: Uri) {
   const session = getSession();
   session.onExecutionLogFn = onExecutionLogFn;
   session.onSessionLogFn = appendSessionLogFn;
+
+  const fileName = basename(codeDoc.getUri(), extname(codeDoc.getUri()));
+  setFileName(fileName);
 
   await session.setup();
 
@@ -220,6 +226,9 @@ async function _runTask(
     appendExecutionLogFn,
   );
   session.onSessionLogFn = appendSessionLogFn;
+
+  const fileName = basename(codeDoc.getUri(), extname(codeDoc.getUri()));
+  setFileName(fileName);
 
   messageEmitter.fire(`${l10n.t("Connecting to SAS session...")}\r\n`);
   !cancelled && (await session.setup(true));
