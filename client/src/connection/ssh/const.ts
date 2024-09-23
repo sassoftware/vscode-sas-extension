04444a735da125568a1bfba2e2cbd5fdedba0875
Joe Morris <Joe.Morris@sas.com>
2024-09-23 11:30:33 -0400

    fix: use a more reasonable unanswered threshhold

--- client/src/connection/ssh/const.ts
+++ client/src/connection/ssh/const.ts
@@ -3,9 +3,9 @@
 
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
-const HOUR = 60 * MINUTE;
 export const KEEPALIVE_INTERVAL = 60 * SECOND; //How often (in milliseconds) to send SSH-level keepalive packets to the server. Set to 0 to disable.
-export const KEEPALIVE_UNANSWERED_THRESHOLD = 12 * HOUR; //How many consecutive, unanswered SSH-level keepalive packets that can be sent to the server before disconnection.
+export const KEEPALIVE_UNANSWERED_THRESHOLD =
+  (15 * MINUTE) / KEEPALIVE_INTERVAL; //How many consecutive, unanswered SSH-level keepalive packets that can be sent to the server before disconnection.
 export const WORK_DIR_START_TAG = "<WorkDirectory>";
 export const WORK_DIR_END_TAG = "</WorkDirectory>";
 export const CONNECT_READY_TIMEOUT = 5 * MINUTE; //allow extra time due to possible prompting
