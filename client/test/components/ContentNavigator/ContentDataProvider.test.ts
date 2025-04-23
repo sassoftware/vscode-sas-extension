6f3297965498e9dafa6ea75ab142e4f7b9cd6bbf
scnnaw <nan.wang@sas.com>
2025-04-23 15:05:57 +0800

    fix:deal with the hash sign

--- client/test/components/ContentNavigator/ContentDataProvider.test.ts
+++ client/test/components/ContentNavigator/ContentDataProvider.test.ts
@@ -24,6 +24,7 @@ import {
   ContentItem,
   ContentSourceType,
 } from "../../../src/components/ContentNavigator/types";
+import { getUpdatedURI } from "../../../src/components/ContentNavigator/utils";
 import SASContentAdapter from "../../../src/connection/rest/SASContentAdapter";
 import { getSasContentUri as getUri } from "../../../src/connection/rest/util";
 import { getUri as getTestUri } from "../../utils";
@@ -172,7 +173,7 @@ describe("ContentDataProvider", async function () {
     const dataProvider = createDataProvider();
 
     const treeItem = await dataProvider.getTreeItem(contentItem);
-    const uri = contentItem.vscUri;
+    const uri = getUpdatedURI(contentItem.vscUri);
     const expectedTreeItem: TreeItem = {
       id: "unique-id",
       label: "testFile",
