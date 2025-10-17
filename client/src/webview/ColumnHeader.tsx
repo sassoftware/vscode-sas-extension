ea9e807a8348d3b63c22a0937718629bd4bab70a
Scott Dover <Scott.Dover@sas.com>
2025-10-17 16:06:43 -0400

    chore: add copyright
    Signed-off-by: Scott Dover <Scott.Dover@sas.com>

--- client/src/webview/ColumnHeader.tsx
+++ client/src/webview/ColumnHeader.tsx
@@ -1,3 +1,6 @@
+// Copyright © 2025, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { useRef } from "react";
 
 import { AgColumn, GridApi } from "ag-grid-community";
