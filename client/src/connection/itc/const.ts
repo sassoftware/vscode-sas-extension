e5378060f56dbbfb023b1344c910a219ca9a3974
Scott Dover <Scott.Dover@sas.com>
2025-10-24 13:52:02 -0400

    chore: cleanup variables
    Signed-off-by: Scott Dover <Scott.Dover@sas.com>

--- client/src/connection/itc/const.ts
+++
@@ -1,9 +0,0 @@
-// Copyright © 2024, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-import env from "./env.json";
-
-export const ERROR_START_TAG = env.Tags.ErrorStartTag;
-export const ERROR_END_TAG = env.Tags.ErrorEndTag;
-
-export const WORK_DIR_START_TAG = env.Tags.WorkDirStartTag;
-export const WORK_DIR_END_TAG = env.Tags.WorkDirEndTag;
--- client/src/extensions.d.ts
+++ client/src/extensions.d.ts
@@ -1,3 +1,6 @@
+// Copyright © 2025, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 declare module "*.ps1" {
   const content: string;
   export default content;
