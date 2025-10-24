23610b4348345041994650265921245d4072b9a7
Scott Dover <Scott.Dover@sas.com>
2025-10-24 10:38:25 -0400

    chore: respond to code review
    Signed-off-by: Scott Dover <Scott.Dover@sas.com>

--- client/src/components/LibraryNavigator/PaginatedResultSet.ts
+++ client/src/components/LibraryNavigator/PaginatedResultSet.ts
@@ -1,6 +1,6 @@
 // Copyright © 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { SortModelItem } from "ag-grid-community";
+import type { SortModelItem } from "ag-grid-community";
 
 class PaginatedResultSet<T> {
   constructor(
--- client/src/connection/rest/RestLibraryAdapter.ts
+++ client/src/connection/rest/RestLibraryAdapter.ts
@@ -1,6 +1,6 @@
 // Copyright © 2024, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { SortModelItem } from "ag-grid-community";
+import type { SortModelItem } from "ag-grid-community";
 import { AxiosResponse } from "axios";
 
 import { getSession } from "..";
