9bc62eb980d84852477230234c68e861801af4ce
Elijah Cúchulainn Reid <56865341+Wizzzzzzard@users.noreply.github.com>
2025-12-16 14:38:54 +0000

    feat: refactoring utils to share logic for python and r

--- server/src/r/utils.ts
+++ server/src/r/utils.ts
@@ -1,90 +1,18 @@
 // Copyright © 2024, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { DocumentSymbol, Position } from "vscode-languageserver";
+import { Position } from "vscode-languageserver";
 import { TextDocument } from "vscode-languageserver-textdocument";
 
-import { CodeZoneManager } from "../sas/CodeZoneManager";
 import { LanguageServiceProvider } from "../sas/LanguageServiceProvider";
-import { isCustomRegionStartComment } from "../sas/utils";
+import { extractEmbeddedLanguageCode } from "../utils/embeddedLanguageUtils";
 
 export const extractRCodes = (
   doc: TextDocument,
   languageService: LanguageServiceProvider,
 ): string => {
-  const codeZoneManager = languageService.getCodeZoneManager();
-  let rDocLines: string[] = [];
-  const symbols: DocumentSymbol[] = languageService.getDocumentSymbols();
-
-  for (let i = 0; i < symbols.length; i++) {
-    const symbol = symbols[i];
-    if (isCustomRegionStartComment(symbol.name)) {
-      symbols.splice(i + 1, 0, ...(symbol.children ?? []));
-    }
-    if (symbol.name?.toUpperCase() !== "PROC RLANG") {
-      continue;
-    }
-
-    let rCodeStart = undefined;
-    let rCodeEnd = undefined;
-    const pos = { ...symbol.range.start };
-
-    while (pos.line <= symbol.range.end.line) {
-      if (
-        !rCodeStart &&
-        codeZoneManager.getCurrentZone(pos.line, pos.character) ===
-          CodeZoneManager.ZONE_TYPE.EMBEDDED_LANG
-      ) {
-        rCodeStart = { ...pos };
-      }
-
-      if (
-        rCodeStart &&
-        codeZoneManager.getCurrentZone(pos.line, pos.character) !==
-          CodeZoneManager.ZONE_TYPE.EMBEDDED_LANG
-      ) {
-        rCodeEnd = { ...pos };
-        if (rCodeEnd.character === 0) {
-          rCodeEnd.line--;
-          rCodeEnd.character =
-            languageService.model.getColumnCount(rCodeEnd.line) - 1;
-          if (rCodeEnd.character < 0) {
-            rCodeEnd.character = 0;
-          }
-        }
-        break;
-      }
-
-      pos.character++;
-      if (pos.character >= languageService.model.getLine(pos.line).length) {
-        pos.line++;
-        pos.character = 0;
-      }
-    }
-
-    if (!rCodeStart) {
-      continue;
-    }
-
-    const rCodeLines = doc
-      .getText({
-        start: rCodeStart,
-        end: rCodeEnd ?? symbol.range.end,
-      })
-      .split("\n");
-
-    const lineGap = rCodeStart.line - rDocLines.length;
-    if (lineGap > 0) {
-      rDocLines = rDocLines.concat(Array(lineGap).fill(""));
-    } else if (lineGap === -1 && rCodeLines[0] === "") {
-      // head in one line: proc rlang; submit;
-      rCodeLines.shift();
-    }
-
-    rDocLines = rDocLines.concat(rCodeLines);
-  }
-
-  const rDoc = rDocLines.join("\n");
-  return rDoc;
+  return extractEmbeddedLanguageCode(doc, languageService, {
+    procName: "PROC RLANG",
+  });
 };
 
 /**
