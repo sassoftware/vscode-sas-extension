f1db59624c3c65de477fbb3a4be0e4471105805b
Scott Dover <Scott.Dover@sas.com>
2026-01-07 15:25:20 -0500

    fix: fix misc selection issues
    Signed-off-by: Scott Dover <Scott.Dover@sas.com>

--- client/src/components/utils/treeViewSelections.ts
+++ client/src/components/utils/treeViewSelections.ts
@@ -1,6 +1,6 @@
 // Copyright © 2024, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { TreeView } from "vscode";
+import { TreeItem, TreeView } from "vscode";
 
 /**
  * Gets the selected items from a tree view based on the current selection state.
@@ -13,11 +13,23 @@ import { TreeView } from "vscode";
  * @param item - The item that was clicked/activated
  * @returns An array of selected items
  */
-export function treeViewSelections<T>(
+export function treeViewSelections<T extends TreeItem>(
   treeView: TreeView<T>,
   item: T | undefined,
 ): T[] {
-  return treeView.selection.length > 1 || !item
-    ? [...treeView.selection]
-    : [item];
+  // If we have an item, we need to make sure it is part of the selection
+  // If it's not, _it_ should be what is selected. Otherwise, we can
+  // just rely on treeView selection
+  if (item) {
+    const itemIsInSelection = treeView.selection.some(
+      ({ id }) => id === item.id,
+    );
+    if (itemIsInSelection) {
+      return [...treeView.selection];
+    }
+
+    return [item];
+  }
+
+  return [...treeView.selection];
 }
