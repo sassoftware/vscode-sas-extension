5579da55dcf9b4eeec20194c713c65ec565ecd0c
Stu Sztukowski <stu.sztukowski@sas.com>
2026-04-07 21:44:37 -0400

    Add copyright per npm copyright run check for pull request #3565
    Signed-off-by: Stu Sztukowski <stu.sztukowski@sas.com>

--- client/src/components/notebook/PythonOutputProcessor.ts
+++ client/src/components/notebook/PythonOutputProcessor.ts
@@ -1,3 +1,5 @@
+// Copyright © 2026, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { LogLine } from "../../connection";
 
 // VS Code's built-in `application/vnd.code.notebook.error` renderer processes.
--- client/src/components/notebook/renderers/PythonErrorRenderer.ts
+++ client/src/components/notebook/renderers/PythonErrorRenderer.ts
@@ -1,3 +1,5 @@
+// Copyright © 2026, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 import type { ActivationFunction } from "vscode-notebook-renderer";
 
 const COLOR_RED = "#e75c58"; // ansi-red-fg  (separator, error type name)
