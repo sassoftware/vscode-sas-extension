8ea3beb13b7dbcd5fa9d06ca2f96d5c826d8b0a2
Scott Dover <Scott.Dover@sas.com>
2026-04-24 09:19:46 -0400

    chore: fix rectangular drawing (left down only)
    Signed-off-by: Scott Dover <Scott.Dover@sas.com>

--- client/src/webview/DataViewer.tsx
+++ client/src/webview/DataViewer.tsx
@@ -181,7 +181,12 @@ const DataViewer = () => {
     dismissSelection,
     rectangleRef,
     ...selectionRectangleHooks
-  } = useSelectionRectangle();
+  } = useSelectionRectangle({
+    scrollContainer: ".ag-body-viewport",
+    scrollBoundaries: () => ({
+      bottom: document.body.clientHeight - 30,
+    }),
+  });
 
   if (columns.length === 0) {
     return null;
@@ -198,12 +203,23 @@ const DataViewer = () => {
       />
       {columnMenu && <ColumnMenu {...columnMenu} />}
       <div
+        className="xy"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: 99999,
+          background: "black",
+        }}
+      >
+        0,0
+      </div>
+      <div
         className={`ag-grid-wrapper ${theme}`}
         style={gridStyles}
         onClick={() => columnMenu && dismissMenuWithoutFocus()}
         {...selectionRectangleHooks}
       >
-        <div className="selection-rectangle" ref={rectangleRef} />
         <AgGridReact
           ref={gridRef}
           cacheBlockSize={100}
