493fb23b0b2b51f7c3c631f14dce609955e35a13
Scott Dover <Scott.Dover@sas.com>
2026-05-01 14:05:06 -0400

    chore: add copyright
    Signed-off-by: Scott Dover <Scott.Dover@sas.com>

--- client/src/components/utils/csv.ts
+++ client/src/components/utils/csv.ts
@@ -1,3 +1,6 @@
+// Copyright © 2026, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 export const stringArrayToCsvString = (strings: string[]): string =>
   `"${strings
     .map((item: string | number) =>
--- client/src/webview/vscode.ts
+++ client/src/webview/vscode.ts
@@ -1,3 +1,6 @@
+// Copyright © 2026, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 declare const acquireVsCodeApi;
 const vscode = acquireVsCodeApi();
 
