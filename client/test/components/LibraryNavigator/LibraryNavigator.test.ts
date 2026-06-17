d057b613dc637356b21f6c382b0ac1acab73cc63
Joe Morris <Joe.Morris@sas.com>
2026-06-17 08:56:32 -0400

    chore: add copyright to new files
    Signed-off-by: Joe Morris <Joe.Morris@sas.com>

--- client/test/components/LibraryNavigator/LibraryNavigator.test.ts
+++ client/test/components/LibraryNavigator/LibraryNavigator.test.ts
@@ -1,3 +1,5 @@
+// Copyright © 2026, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { expect } from "chai";
 import { timingSafeEqual } from "crypto";
 import { EventEmitter } from "events";
