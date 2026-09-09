b980ff81259b595a5bd200990a3d18c661ed8177
Nurquhart <Nurquhart014@gmail.com>
2026-09-09 09:35:52 -0400

    fix: sas notebook filename
    sas notebook names in sas content and server were not
    being correctly identified,
    which caused issues for the _SASPROGRAMFILE variable
    
    Signed-off-by: Nurquhart <Nurquhart014@gmail.com>

--- client/test/components/util/SASCodeDocumentHelper.test.ts
+++ client/test/components/util/SASCodeDocumentHelper.test.ts
@@ -82,6 +82,46 @@ describe("sas code document helper", () => {
       getWordRangeAtPosition: () => undefined,
     };
 
+    const mockNotebookDocument = {
+      languageId: "sas",
+      getText: function () {
+        return "data _null_; run;";
+      },
+      uri: vscode.Uri.parse(
+        "vscode-notebook-cell:/sasNotebookServer.sasnb?id%3D%2Fcompute%2Fsessions%2F63d47209-f7cc-47df-b75c-605578b59a0f-ses0000%2Ffiles%2F~fs~dmtesting~fs~naurqu~fs~sasNotebookServer.sasnb#W0sc2FzU2VydmVy",
+      ),
+      fileName: "/sasNotebookServer.sasnb",
+      lineCount: 1,
+      lineAt: function (lineOrPos: number | vscode.Position = 0) {
+        const lineNumber =
+          typeof lineOrPos === "number" ? lineOrPos : lineOrPos.line;
+        return {
+          lineNumber,
+          text: "data _null_; run;",
+          range: new vscode.Range(lineNumber, 0, lineNumber, 16),
+          rangeIncludingLineBreak: new vscode.Range(
+            lineNumber,
+            0,
+            lineNumber,
+            17,
+          ),
+          firstNonWhitespaceCharacterIndex: 0,
+          isEmptyOrWhitespace: false,
+        };
+      },
+      isUntitled: false,
+      version: 1,
+      isDirty: false,
+      isClosed: false,
+      save: async () => true,
+      eol: 1,
+      offsetAt: () => 0,
+      positionAt: () => new vscode.Position(0, 0),
+      validateRange: (range: vscode.Range) => range,
+      validatePosition: (pos: vscode.Position) => pos,
+      getWordRangeAtPosition: () => undefined,
+    };
+
     it("should construct parameters with default values", () => {
       const params = getCodeDocumentConstructionParameters(mockTextDocument);
       assert.equal(params.languageId, "sas");
@@ -118,6 +158,16 @@ describe("sas code document helper", () => {
       const params = getCodeDocumentConstructionParameters(mockTextDocument);
       // the results.html.style should be applied
       assert.equal(params.htmlStyle, "HTMLBlue");
+    });
+
+    it("should generate correct file name for sas notebook", () => {
+      const params =
+        getCodeDocumentConstructionParameters(mockNotebookDocument);
+      // the results.html.style should be applied
+      assert.equal(
+        params.fileName,
+        "/dmtesting/naurqu/sasNotebookServer.sasnb",
+      );
     });
   });
 });
